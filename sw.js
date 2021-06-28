89fc6b12b7a69465e16e8692540eb9bc3463d6fd
Philippe Rigovanov <phil@rigovanov.ru>
2021-06-28 15:57:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
 ---
 ---
-const staticCacheName = 'static-cache-rigovanov-songs-v010';
-const dynamicCacheName = 'dynamic-cache-rigovanov-songs-v010';
+const staticCacheName = 'static-cache-rigovanov-songs-v011';
+const dynamicCacheName = 'dynamic-cache-rigovanov-songs-v011';
 
 const staticAssets = [
     '{{ "/" | relative_url }}',
     '{{ "/alphabet/" | relative_url }}',
-    {% for post in site.posts %}'{{ post.url | relative_url }}',
+    {% for post in site.posts -%}
+    '{{ post.url | relative_url }}',
     {% endfor -%}
     {% assign audio_files = site.static_files | where: "audio", true -%}
-    {% for myaudio in audio_files %}
-    {{ myaudio.path | relative_url }}
+    {% for myaudio in audio_files -%}
+    '{{ myaudio.path | relative_url }}',
     {% endfor -%}
     '{{ "/offline/" | relative_url }}',
-    {% if site.minimal %}'{{ "/assets/css/minimal.css" | relative_url }}',
+    {% if site.minimal -%}
+    '{{ "/assets/css/minimal.css" | relative_url }}',
     '{{ "/assets/css/classes.css" | relative_url }}',
-    {% else %}'{{ "/assets/css/style.css" | relative_url }}',{% endif -%}
-    {% if site.sidebar %}'{{ "/assets/css/sidebar.css" | relative_url }}',{% endif %}
+    {% else -%}
+    '{{ "/assets/css/style.css" | relative_url }}',
+    {%- endif -%}
+    {% if site.sidebar -%}
+    '{{ "/assets/css/sidebar.css" | relative_url }}',
+    {%- endif %}
     '{{ "/assets/fonts/PTSans-Regular.woff" | relative_url }}',
     '{{ "/assets/fonts/PTSans-Bold.woff" | relative_url }}',
     '{{ "/images/icons/hymnbook.svg" | relative_url }}',
