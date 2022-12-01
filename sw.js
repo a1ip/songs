25e5e5b3b1fbe99e67b32f3062337ec04d9c6af2
Philippe Rigovanov <phil@rigovanov.ru>
2022-12-01 12:58:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 ---
-const staticCacheName = 'static-cache-rigovanov-songs-v099';
-const dynamicCacheName = 'dynamic-cache-rigovanov-songs-v099';
+const staticCacheName = 'static-cache-rigovanov-songs-v100';
+const dynamicCacheName = 'dynamic-cache-rigovanov-songs-v100';
 
 const staticAssets = [
     '{{ "/" | relative_url }}',
